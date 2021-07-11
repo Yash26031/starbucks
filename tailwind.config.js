7f63671298a3239e26ba7f93982f319aef645228
Yash <85996457+Yash26031@users.noreply.github.com>
2021-07-11 13:51:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary:{
+        100: "#f1f8f6",
+        200: "#d4e9e2",
+        300: "#008248",
+        400: "#1e3932",
+      },
+      secondary: "#f3f1e7",
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
